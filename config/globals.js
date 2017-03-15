7ed61a386c44fb401e5a1c19409f6b2f95946c42
Artem Iermak <outpacer@ex.ua>
2017-03-15 15:21:50 -0400

    first commit

---
+++ config/globals.js
@@ -0,0 +1,9 @@
+/**
+ * Created by RFreeman on 2/8/2017.
+ */
+
+// create an array of global variables
+module.exports = {
+  // db: 'mongodb://localhost/comp2106-w2017' // local mongodb (no auth)
+    db: 'mongodb://comp2106artemdb:Artem+123@ds147079.mlab.com:47079/comp2106artem' //mlab
+};
